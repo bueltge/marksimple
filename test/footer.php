c2d2b1fb34925744275b360606a60980ce768402
Frank Bültge <frank@bueltge.de>
2017-09-05 13:23:55 +0200

    Separate index to reuse parts of site

---
+++ test/footer.php
@@ -0,0 +1,14 @@
+<?php # -*- coding: utf-8 -*-
+declare( strict_types = 1 );
+?>
+
+		<footer class="wrapper">
+		<?php
+			// Parse the Markdown content from a string.
+			print ( new MS( 'Made with ♥ · [bueltge.de](https://bueltge.de).' ) );
+		?>
+		</footer>
+
+		<script src="js/main.js"></script>
+	</body>
+</html>
--- test/index.php
+++ test/index.php
@@ -3,47 +3,9 @@ declare( strict_types = 1 );
 
 require_once '../MarkSimple.php';
 use Bueltge\MarkSimple\MarkSimple as MS;
+
+require_once 'head.php';
 ?>
-<!doctype html>
-<html class="no-js" lang="en">
-	<head>
-		<meta charset="utf-8">
-		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-		<title>MarkSimple Tests and Examples</title>
-		<meta name="description" content="">
-		<meta name="viewport" content="width=device-width, initial-scale=1">
-		
-		<!-- Web Application Manifest -->
-		<link rel="manifest" href="manifest.json">
-		
-		<!-- Add to homescreen for Chrome on Android -->
-    <meta name="mobile-web-app-capable" content="yes">
-    <meta name="application-name" content="MarkSimple Tests and Examples">
-    <link rel="icon" sizes="192x192" href="images/icon.png">
-
-    <!-- Add to homescreen for Safari on iOS -->
-    <meta name="apple-mobile-web-app-capable" content="yes">
-    <meta name="apple-mobile-web-app-status-bar-style" content="black">
-    <meta name="apple-mobile-web-app-title" content="Web Starter Kit">
-    <link rel="apple-touch-icon" href="images/icon.png">
-
-    <!-- Tile icon for Win8 (144x144 + tile color) -->
-    <meta name="msapplication-TileImage" content="images/icon.png">
-    <meta name="msapplication-TileColor" content="#2F3BA2">
-
-    <!-- Color the status bar on mobile devices -->
-    <meta name="theme-color" content="#FFFFFF">
-		
-		<link rel="apple-touch-icon" href="images/icon.png">
-
-		<link rel="preload" as="style" href="css/normalize.min.css" onload="this.rel='stylesheet'">
-		<link rel="preload" as="style" href="css/main.css" onload="this.rel='stylesheet'">
-
-		<!--[if lt IE 9]>
-			<script src="js/vendor/html5-3.6-respond-1.4.2.min.js"></script>
-		<![endif]-->
-	</head>
-	<body>
 
 		<div class="main">
 			<article>
@@ -54,13 +16,4 @@ use Bueltge\MarkSimple\MarkSimple as MS;
 			</article>
 		</div> <!-- .main -->
 
-		<footer class="wrapper">
-		<?php
-			// Parse the Markdown content from a string.
-			print ( new MS( 'Made with ♥ · [bueltge.de](http://bueltge.de).' ) );
-		?>
-		</footer>
-
-		<script src="js/main.js"></script>
-	</body>
-</html>
+<?php require_once 'footer.php'; ?>
