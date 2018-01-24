2eca6653afc9e6916d81c746979ff5081a1ee385
Frank Bueltge <frank@bueltge.de>
2018-01-24 17:54:34 +0100

    Enhance tests for h1

--- tests/phpunit/Unit/MarkSimpleTest.php
+++ tests/phpunit/Unit/MarkSimpleTest.php
@@ -7,16 +7,34 @@ use MyProject\Container;
 
 class MarkSimpleTest extends AbstractTestCase {
 
+	/**
+	 * Instance of ?
+	 */
 	public function test_basic() {
 
 		$testee = new MarkSimple( '' );
 		static::assertInstanceOf( Container::class, $testee );
 	}
 
+	/**
+	 * Test constructor.
+	 */
 	public function test_construct() {
 
 		$content = '';
 		$testee = new MarkSimple( $content );
 		static::assertSame( $content, $testee );
+	}
+
+	/**
+	 * Test h1 include add if an id to h1 markup.
+	 */
+	public function test_header() {
+
+		$content = '# I like this library.';
+		$result  = '<h1 id="greatplayground">Great playground.</h1>';
+
+		$testee = new MarkSimple( $content );
+		static::assertSame( $result, $testee );
 	}
 }
