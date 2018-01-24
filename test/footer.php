f6213642f00082ab8b35320baed8ec43d79b89c4
Frank Bueltge <frank@bueltge.de>
2018-01-24 17:41:57 +0100

    fix include for new strucure

--- test/footer.php
+++ test/footer.php
@@ -4,7 +4,7 @@ declare( strict_types = 1 );
 
 		<footer class="wrapper">
 		<?php
-		require_once '../MarkSimple.php';
+		require_once '../src/MarkSimple.php';
 		use Bueltge\MarkSimple\MarkSimple as MS;
 			// Parse the Markdown content from a string.
 			print ( new MS( 'Made with ♥ · [bueltge.de](https://bueltge.de).' ) );
