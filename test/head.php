6f6be91d57a77d484749a656c774b43f1ef5000e
Frank Bültge <frank@bueltge.de>
2019-06-03 13:47:43 +0200

    fix redundant

--- test/head.php
+++ test/head.php
@@ -7,7 +7,6 @@ declare(strict_types=1);
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
     <title>MarkSimple Tests and Examples</title>
-    <meta name="theme-color" content="#FFFFFF"/>
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
